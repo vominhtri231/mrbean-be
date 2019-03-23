2d604e1501c87160b9165966fe5c1359f4e4bc8e
TriMinhVo <Tri.Minh.Vo@mgm-tp.com>
2019-03-23 21:42:56 +0700

    Create hello world app

---
+++ src/main/java/school/finalproject/mrbbe/MrbBeApplication.java
@@ -0,0 +1,13 @@
+package school.finalproject.mrbbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrbBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrbBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/school/finalproject/mrbbe/MrbBeApplicationTests.java
@@ -0,0 +1,16 @@
+package school.finalproject.mrbbe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MrbBeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
